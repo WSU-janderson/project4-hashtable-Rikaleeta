513d1289103e0b163de793b0a8b39732e2ddb873
AmandaMilovich <amandaamilovich@gmail.com>
2025-11-02 23:52:37 -0500

    copy-pasted method declarations

--- HashTable.cpp
+++ HashTable.cpp
@@ -1,3 +1,41 @@
-/**
- * HashTable.cpp
- */
+#include "HashTable.h"
+
+HashTable::HashTable(size_t initCapacity) {
+
+}
+
+bool HashTable::insert(const std::string& key, const size_t &value) {
+
+}
+
+bool HashTable::remove(const std::string &key) {
+
+}
+
+bool HashTable::contains(const std::string &key) const {
+
+}
+
+std::optional<size_t> HashTable::get(const std::string &key) const {
+
+}
+
+size_t &HashTable::operator[](const std::string &key) {
+
+}
+
+std::vector<std::string> HashTable::keys() const {
+
+}
+
+double HashTable::alpha() const {
+
+}
+
+size_t HashTable::capacity() const {
+
+}
+
+size_t HashTable::size() const {
+
+}
--- HashTable.h
+++ HashTable.h
@@ -1,3 +1,50 @@
 /**
- * HashTable.h
- */
+ * Project: Hash Table
+ * Amanda Milovich
+*/
+
+#pragma once
+#include <string>
+#include <optional>
+#include <vector>
+#include <ostream>
+
+
+class HashTable {
+public:
+    HashTable(size_t initCapacity = 8);
+    bool insert(const std::string &key, const size_t &value);
+    bool remove(const std::string &key);
+    bool contains(const std::string &key) const;
+    std::optional<size_t> get(const std::string &key) const;
+    size_t &operator[](const std::string &key);
+    std::vector<std::string> keys() const;
+    double alpha() const;
+    size_t capacity() const;
+    size_t size() const;
+
+private:
+    enum class BucketType {
+        NORMAL,
+        EMPTY_SINCE_START,
+        EMPTY_SINCE_REMOVAL
+    };
+
+    class HashTableBucket {
+    public:
+        HashTableBucket();
+        HashTableBucket(const std::string &key, const size_t &value);
+        void load(const std::string &key, const size_t &value);
+        bool isEmpty() const;
+        friend std::ostream &operator<<(std::ostream &os, const HashTableBucket &bucket);
+        std::string getKey() const;
+        size_t getValue() const;
+
+    private:
+        std::string key;
+        size_t value;
+        BucketType type;
+    };
+};
+
+

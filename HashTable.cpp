7282669e4924c3859b2b6e453f37ed92b0f8c7cf
AmandaMilovich <amandaamilovich@gmail.com>
2025-11-03 19:43:07 -0500

    wrote almost all the methods because im tired and forgot to commit

--- HashTable.cpp
+++ HashTable.cpp
@@ -1,41 +1,136 @@
 #include "HashTable.h"
 
-HashTable::HashTable(size_t initCapacity) {
+#include <algorithm>
+#include <random>
+#include <bits/locale_facets_nonio.h>
 
+HashTable::HashTable(size_t initCapacity)
+    : buckets(initCapacity),
+      offsets(initCapacity),
+      fullBuckets(0) {
+    std::iota(offsets.begin(), offsets.end(), 0);
+    std::shuffle(++offsets.begin(), offsets.end(), std::mt19937(std::random_device()()));
 }
 
-bool HashTable::insert(const std::string& key, const size_t &value) {
+size_t HashTable::probe(std::string key, size_t i) const {
+    size_t hashedKey = hasher(key);
+    hashedKey = (hashedKey + offsets[i]) % buckets.size();
+    return hashedKey;
+}
 
+bool HashTable::insert(const std::string &key, const size_t &value) {
+    if (contains(key))
+        return false;
+    bool inserted = false;
+    for (int i = 0; !inserted && i < buckets.size(); i++) {
+        size_t insertBucket = probe(key, i);
+        if (buckets[insertBucket].isEmpty()) {
+            buckets[insertBucket].load(key, value);
+            fullBuckets++;
+            inserted = true;
+        }
+    }
+    return inserted;
 }
 
 bool HashTable::remove(const std::string &key) {
-
+    bool removed = false;
+    for (int i = 0; !removed && i < buckets.size(); i++) {
+        size_t removeBucket = probe(key, i);
+        if (buckets[removeBucket].getKey() == key) {
+            buckets[removeBucket].kill();
+            fullBuckets--;
+            removed = true;
+        }
+    }
+    return removed;
 }
 
 bool HashTable::contains(const std::string &key) const {
-
+    bool found = false;
+    for (int i = 0; !found && i < buckets.size(); i++) {
+        size_t keyHash = probe(key, i);
+        if (buckets[keyHash].getKey() == key)
+            found = true;
+    }
+    return found;
 }
 
 std::optional<size_t> HashTable::get(const std::string &key) const {
-
+    if (!contains(key))
+        return std::nullopt;
+    for (int i = 0; i < buckets.size(); i++) {
+        size_t keyHash = probe(key, i);
+        if (buckets[keyHash].getKey() == key)
+            return std::make_optional(buckets[keyHash].getValue());
+    }
+    return std::nullopt;
 }
 
 size_t &HashTable::operator[](const std::string &key) {
-
+    if (!contains(key))
+        insert(key, 0);
+    for (int i = 0; i < buckets.size(); i++) {
+        size_t keyHash = probe(key, i);
+        if (buckets[keyHash].getKey() == key)
+            return *buckets[keyHash];
+    }
 }
 
 std::vector<std::string> HashTable::keys() const {
-
+    std::vector<std::string> keys;
+    for (int i = 0; i < buckets.size(); i++) {
+        if (buckets[i].isEmpty())
+            continue;
+        keys.push_back(buckets[i].getKey());
+    }
+    return keys;
 }
 
 double HashTable::alpha() const {
-
+    return static_cast<double>(fullBuckets) / static_cast<double>(buckets.size());
 }
 
 size_t HashTable::capacity() const {
-
+    return buckets.size();
 }
 
 size_t HashTable::size() const {
-
+    return fullBuckets;
 }
+
+size_t &HashTable::HashTableBucket::operator*() {
+    return value;
+}
+
+HashTable::HashTableBucket::HashTableBucket()
+    : key(""),
+      value(0),
+      type(BucketType::EMPTY_SINCE_START) {
+}
+
+HashTable::HashTableBucket::HashTableBucket(const std::string &key, const size_t &value)
+    : key(key),
+      value(value),
+      type(BucketType::EMPTY_SINCE_START) {
+}
+
+void HashTable::HashTableBucket::load(const std::string &newKey, const size_t &newValue) {
+    key = newKey;
+    value = newValue;
+    type = BucketType::NORMAL;
+}
+
+void HashTable::HashTableBucket::kill() {
+    key = "";
+    value = 0;
+    type = BucketType::EMPTY_SINCE_REMOVAL;
+}
+
+bool HashTable::HashTableBucket::isEmpty() const {
+    return type != BucketType::NORMAL;
+}
+
+
+
+
--- HashTable.h
+++ HashTable.h
@@ -9,19 +9,18 @@
 #include <vector>
 #include <ostream>
 
-
 class HashTable {
 public:
-    HashTable(size_t initCapacity = 8);
+    explicit HashTable(size_t initCapacity = 8);
     bool insert(const std::string &key, const size_t &value);
     bool remove(const std::string &key);
     bool contains(const std::string &key) const;
-    std::optional<size_t> get(const std::string &key) const;
+    [[nodiscard]] std::optional<size_t> get(const std::string &key) const;
     size_t &operator[](const std::string &key);
-    std::vector<std::string> keys() const;
-    double alpha() const;
-    size_t capacity() const;
-    size_t size() const;
+    [[nodiscard]] std::vector<std::string> keys() const;
+    [[nodiscard]] double alpha() const;
+    [[nodiscard]] size_t capacity() const;
+    [[nodiscard]] size_t size() const;
 
 private:
     enum class BucketType {
@@ -30,21 +29,31 @@ private:
         EMPTY_SINCE_REMOVAL
     };
 
+    size_t probe(std::string key, size_t i) const;
+
     class HashTableBucket {
     public:
         HashTableBucket();
         HashTableBucket(const std::string &key, const size_t &value);
         void load(const std::string &key, const size_t &value);
+        void kill();
         bool isEmpty() const;
         friend std::ostream &operator<<(std::ostream &os, const HashTableBucket &bucket);
         std::string getKey() const;
         size_t getValue() const;
+        size_t &operator*();
 
     private:
         std::string key;
         size_t value;
         BucketType type;
     };
+
+    std::vector<HashTableBucket> buckets;
+    std::vector<size_t> offsets;
+    std::hash<std::string> hasher;
+    size_t fullBuckets;
+
 };
 
 
